a146dfa5a646297c5bd321f738a92ba260f6b7a5
ToteBrick <402981948@qq.com>
2019-07-08 19:36:24 +0800

    设置session有效期

--- session.py
+++ session.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+一、session的设置
+二、flask的session存放的位置（flask将session放在cookie中，flask中的session为浏览器session）
+    可以引用flask-session指定session的储存位置
+三、需求：如何删除session和设置session的有效时间, 以及session默认存储时间？？？
+    flask默认存储时间：当浏览器关闭。 （31天，需要依赖于开启配置）
+
+"""
 from flask import Flask, session
 
 app = Flask(__name__)
@@ -24,6 +32,17 @@ def get_session():
     # 获取session
     username = session.get('user', None)
     return 'username is {}.'.format(username)
+
+
+# 设置session的有效期
+@app.route("/session")
+def set_session_time():
+    session['name'] = 'zhangsan'
+    session.permanent = True
+    # 指定session的过期时间
+    app.permanent_session_lifetime = 86400
+    return "ok"
+
 
 # 删除session
 @app.route('/delete')
